aff4bd1fe8ae8c1163584ed4c0696ba36dc01c58
Teresa <mjimenezni@educa.jcyl.es>
2021-12-27 16:53:07 +0100

    Third Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +13,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 import { PokemonsComponent } from './components/pokemons/pokemons.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, PokemonsComponent, PokemonComponent],
@@ -26,6 +29,13 @@ import { PokemonsComponent } from './components/pokemons/pokemons.component';
     MatIconModule,
     MatProgressBarModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/pokemons/pokemons.component.ts
+++ src/app/components/pokemons/pokemons.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { PokemonList } from 'src/app/models/pokemon.interface';
 import { PokemonsService } from 'src/app/services/pokemons.service';
@@ -6,15 +13,32 @@ import { PokemonsService } from 'src/app/services/pokemons.service';
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0.6,
+        })
+      ),
+      transition('void<=>*', animate(1800)),
+    ]),
+  ],
 })
 export class PokemonsComponent implements OnInit {
   pokemonList: PokemonList = { count: 0, results: [{ name: '', url: '' }] };
   displayedColumns: string[] = ['name', 'url'];
+  isLoading: boolean = true;
+
   constructor(private pokemonsService: PokemonsService) {}
 
   ngOnInit(): void {
-    this.pokemonsService
-      .getAllPokemons()
-      .subscribe((pokemonList) => (this.pokemonList = pokemonList));
+    this.pokemonsService.getAllPokemons().subscribe(
+      (pokemonList) => {
+        this.isLoading = false;
+        this.pokemonList = pokemonList;
+      },
+      (error) => (this.isLoading = true)
+    );
   }
 }
